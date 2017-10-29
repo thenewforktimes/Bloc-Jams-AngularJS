db83ff5bb40bc189e8c0222ddadb9a3c21559a4b
balticoceanblue <robert.w.ballard@gmail.com>
2017-10-29 16:48:44 -0700

    sanity check checkpoint 7 broken buttons

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,40 @@
+(function() {
+     function SongPlayer() {
+       var SongPlayer = {};
+       var currentSong = null;
+       var currentBuzzObject = null;
+
+     SongPlayer.play = function(song) {
+       if (currentSong !== song) {
+         if (currentBuzzObject) {
+           currentBuzzObject.stop();
+           currentSong.playing = null;
+         }
+         else if (currentSong === song) {
+         if (currentBuzzObject.isPaused()) {
+              currentBuzzObject.play();
+           }
+         }
+           currentBuzzObject = new buzz.sound(song.audioUrl, {
+           formats: ['mp3'],
+           preload: true
+           });
+
+         currentSong = song;
+
+         currentBuzzObject.play();
+         song.playing = true;
+       }
+     };
+
+     SongPlayer.pause = function(song) {
+       currentBuzzObject.pause();
+       song.playing = false;
+ };
+       return SongPlayer;
+};
+
+     angular
+         .module('blocJams')
+         .factory('SongPlayer', SongPlayer);
+ })();
