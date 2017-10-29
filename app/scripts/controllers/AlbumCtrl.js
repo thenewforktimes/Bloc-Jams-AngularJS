ff56bc69d66b39c755dcef57c22e4980be9ed16a
balticoceanblue <robert.w.ballard@gmail.com>
2017-10-28 23:26:23 -0700

    sanity check on getCollection() method

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,11 @@
 (function() {
-  function AlbumCtrl() {
-    this.albumData = angular.copy(albumPicasso);
-      
+  function AlbumCtrl(Fixtures) {
+    this.albumData = Fixtures.getAlbum();
+
     }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', [AlbumCtrl])
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl])
 
 })();
