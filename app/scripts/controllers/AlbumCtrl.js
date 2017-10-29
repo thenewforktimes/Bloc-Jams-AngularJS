eab4221dbc4e528cfd37221786974f15b15a3224
balticoceanblue <robert.w.ballard@gmail.com>
2017-10-28 18:29:46 -0700

    checkpoint 5 controllers completed

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+  function AlbumCtrl() {
+    this.albumData = angular.copy(albumPicasso);
+      
+    }
+
+  angular
+    .module('blocJams')
+    .controller('AlbumCtrl', [AlbumCtrl])
+
+})();
